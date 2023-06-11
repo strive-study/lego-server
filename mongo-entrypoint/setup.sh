aa837134022fc5e85c0dd4f73e257b06c954f185
xiaoli <2585960098@qq.com>
2023-06-11 22:26:45 +0800

    chore:增加mongodb启动shell脚本，redis配置修改

---
+++ mongo-entrypoint/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e 
+mongo <<EOF
+use admin
+db.auth('$MONGO_INITDB_ROOT_USERNAME','$MONGO_INITDB_ROOT_PASSWORD')
+use lego
+db.createUser({
+  user:'$MONGO_DB_USERNAME',
+  pwd:'$MONGO_DB_PASSWORD',
+  roles:[{
+    role:'readWrite',
+    db:'lego'
+  }]
+})
+
+db.createCollection('works')
+db.works.insertMany([
+  {
+    id: 19,
+    title: '1024 程序员日',
+    desc: '1024 程序员日',
+    author: '185****2625',
+    coverImg: 'http://static-dev.imooc-lego.com/imooc-test/sZHlgv.png',
+    copiedCount: 737,
+    isHot: true,
+    isTemplate: true,
+    isPublic: true,
+    createdAt: '2020-11-26T09:27:19.000Z',
+  }
+])
+EOF
